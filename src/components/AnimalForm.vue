65eda1817ec4c956ef5eb5f8f07f4f9e7b44836b
Aristevao <aristevao@hotmail.com>
2024-07-12 20:39:59 -0300

    Add all fields to animals form

--- src/components/AnimalForm.vue
+++ src/components/AnimalForm.vue
@@ -1,38 +1,62 @@
-<!-- AnimalForm.vue -->
 <template>
-    <transition name="modal">
-      <div class="animal-form-modal" v-if="isVisible">
-        <div class="animal-form">
-          <h2>Create New Animal</h2>
-          <form @submit.prevent="submitForm">
-            <!-- Form fields for animal creation -->
-            <label>Name:</label>
-            <input v-model="formData.name" required />
-  
-            <label>Species:</label>
-            <input v-model="formData.species" required />
-  
-            <!-- Add more fields as needed -->
-  
-            <div class="button-group">
-              <button type="submit">Create Animal</button>
-              <button type="button" @click="cancelForm">Cancel</button>
-            </div>
-          </form>
-        </div>
+  <transition name="modal">
+    <div class="animal-form-modal" v-if="isVisible">
+      <div class="animal-form">
+        <h2>Create New Animal</h2>
+        <form @submit.prevent="submitForm">
+          <!-- Form fields for animal creation -->
+          <label>Name:</label>
+          <input v-model="formData.name" required />
+
+          <label>Identification:</label>
+          <input v-model="formData.identification" required />
+
+          <label>Specie:</label>
+          <input v-model="formData.specie" required />
+
+          <label>Breed:</label>
+          <input v-model="formData.breed" required />
+
+          <label>Sex:</label>
+          <input v-model="formData.sex" required />
+
+          <label>Birthdate:</label>
+          <input v-model="formData.birthdate" required />
+
+          <label>Registration Date:</label>
+          <input v-model="formData.registrationDate" required />
+
+          <label>Description:</label>
+          <input v-model="formData.description" required />
+
+          <label>Fazenda:</label>
+          <input v-model="formData.unit" required />
+
+          <div class="button-group">
+            <button type="submit">Create Animal</button>
+            <button type="button" @click="cancelForm">Cancel</button>
+          </div>
+        </form>
       </div>
-    </transition>
-  </template>
-  
-  <script>
+    </div>
+  </transition>
+</template>
+
+<script>
   export default {
     data() {
       return {
-        isVisible: false,
+        isVisible: true, // TODO: Default = false. True exige 2 clicks pra abrir a modal
         formData: {
           name: '',
-          species: '',
-          // Add more fields as needed
+          identification: '',
+          specie: '',
+          breed: '',
+          sex: '',
+          birthdate: '',
+          registrationDate: '',
+          description: '',
+          unit: '',
         }
       };
     },
@@ -54,14 +78,21 @@
       },
       resetForm() {
         this.formData.name = '';
-        this.formData.species = '';
-        // Reset other fields as needed
+        this.formData.identification = '';
+        this.formData.specie = '';
+        this.formData.breed = '';
+        this.formData.sex = '';
+        this.formData.birthdate = '';
+        this.formData.registrationDate = '';
+        this.formData.description = '';
+        this.formData.unit = '';
       }
     }
   };
-  </script>
-  
-  <style scoped>
+</script>
+
+<style scoped>
+
   /* Add scoped styles for AnimalForm.vue */
   .animal-form-modal {
     position: fixed;
@@ -75,7 +106,7 @@
     align-items: center;
     z-index: 999;
   }
-  
+
   .animal-form {
     background-color: white;
     padding: 20px;
@@ -84,25 +115,24 @@
     max-width: 400px;
     width: 100%;
   }
-  
+
   .button-group {
     margin-top: 20px;
     text-align: right;
   }
-  
+
   .button-group button {
     margin-left: 10px;
   }
-  
+
   /* Modal transition */
   .modal-enter-active,
   .modal-leave-active {
     transition: opacity 0.3s;
   }
-  
+
   .modal-enter,
   .modal-leave-to {
     opacity: 0;
   }
-  </style>
-  
+</style>
