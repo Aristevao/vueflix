ac632c6bf9ea9616b18b1547ff61d5f163ecc21d
Aristevao <aristevao@hotmail.com>
2024-07-12 21:03:00 -0300

    Send the form to create an animal

--- src/components/AnimalForm.vue
+++ src/components/AnimalForm.vue
@@ -3,34 +3,39 @@
     <div class="animal-form-modal" v-if="isVisible">
       <div class="animal-form">
         <h2>Create New Animal</h2>
-        <form @submit.prevent="submitForm">
-          <!-- Form fields for animal creation -->
+        <form @submit.prevent="submitForm" enctype="multipart/form-data">
           <label>Name:</label>
-          <input v-model="formData.name" required />
+          <input v-model="formData.name" maxlength="80" />
 
           <label>Identification:</label>
-          <input v-model="formData.identification" required />
+          <input v-model="formData.identification" required maxlength="80" />
 
           <label>Specie:</label>
-          <input v-model="formData.specie" required />
+          <input v-model="formData.specie" maxlength="80" />
 
           <label>Breed:</label>
-          <input v-model="formData.breed" required />
+          <input v-model="formData.breed" maxlength="80" />
 
           <label>Sex:</label>
-          <input v-model="formData.sex" required />
+          <select v-model="formData.sex">
+            <option>MALE</option>
+            <option>FEMALE</option>
+          </select>
 
           <label>Birthdate:</label>
-          <input v-model="formData.birthdate" required />
+          <input type="date" v-model="formData.birthdate" />
 
           <label>Registration Date:</label>
-          <input v-model="formData.registrationDate" required />
+          <input type="date" v-model="formData.registrationDate" />
 
           <label>Description:</label>
-          <input v-model="formData.description" required />
+          <textarea v-model="formData.description" maxlength="500"></textarea>
 
-          <label>Fazenda:</label>
-          <input v-model="formData.unit" required />
+          <label>Unit ID:</label>
+          <input type="number" v-model="formData.unitId" required />
+
+          <label>Picture:</label>
+          <input type="file" @change="handleFileUpload" />
 
           <div class="button-group">
             <button type="submit">Create Animal</button>
@@ -43,96 +48,125 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        isVisible: true, // TODO: Default = false. True exige 2 clicks pra abrir a modal
-        formData: {
-          name: '',
-          identification: '',
-          specie: '',
-          breed: '',
-          sex: '',
-          birthdate: '',
-          registrationDate: '',
-          description: '',
-          unit: '',
-        }
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      isVisible: true,// TODO: Default = false. True exige 2 clicks pra abrir a modal
+      formData: {
+        name: '',
+        identification: '',
+        specie: '',
+        breed: '',
+        sex: '',
+        birthdate: '',
+        registrationDate: '',
+        description: '',
+        unitId: '',
+        picture: null,
+      },
+    };
+  },
+  methods: {
+    open() {
+      this.isVisible = true;
+    },
+    close() {
+      this.isVisible = false;
+      this.resetForm();
+    },
+    handleFileUpload(event) {
+      this.formData.picture = event.target.files[0];
+    },
+    submitForm() {
+      const formData = new FormData();
+      formData.append('name', this.formData.name);
+      formData.append('identification', this.formData.identification);
+      formData.append('specie', this.formData.specie);
+      formData.append('breed', this.formData.breed);
+      formData.append('sex', this.formData.sex);
+      formData.append('birthdate', this.formData.birthdate);
+      formData.append('registrationDate', this.formData.registrationDate);
+      formData.append('description', this.formData.description);
+      formData.append('unit.id', this.formData.unitId);
+      if (this.formData.picture) {
+        formData.append('picture', this.formData.picture);
+      }
+
+      axios.post('http://localhost:8080/api/digital-pec/animal', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+      .then(response => {
+        this.$emit('animal-created', response.data);
+        this.close();
+      })
+      .catch(error => {
+        console.error('Error creating animal:', error);
+      });
+    },
+    cancelForm() {
+      this.close();
+    },
+    resetForm() {
+      this.formData = {
+        name: '',
+        identification: '',
+        specie: '',
+        breed: '',
+        sex: '',
+        birthdate: '',
+        registrationDate: '',
+        description: '',
+        unitId: '',
+        picture: null,
       };
     },
-    methods: {
-      open() {
-        this.isVisible = true;
-      },
-      close() {
-        this.isVisible = false;
-        this.resetForm();
-      },
-      submitForm() {
-        // Logic to submit form and create new animal
-        this.$emit('animal-created', this.formData);
-        this.close();
-      },
-      cancelForm() {
-        this.close();
-      },
-      resetForm() {
-        this.formData.name = '';
-        this.formData.identification = '';
-        this.formData.specie = '';
-        this.formData.breed = '';
-        this.formData.sex = '';
-        this.formData.birthdate = '';
-        this.formData.registrationDate = '';
-        this.formData.description = '';
-        this.formData.unit = '';
-      }
-    }
-  };
+  },
+};
 </script>
 
 <style scoped>
+.animal-form-modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
 
-  /* Add scoped styles for AnimalForm.vue */
-  .animal-form-modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 999;
-  }
+.animal-form {
+  background-color: white;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  max-width: 400px;
+  width: 100%;
+}
 
-  .animal-form {
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    max-width: 400px;
-    width: 100%;
-  }
+.button-group {
+  margin-top: 20px;
+  text-align: right;
+}
 
-  .button-group {
-    margin-top: 20px;
-    text-align: right;
-  }
+.button-group button {
+  margin-left: 10px;
+}
 
-  .button-group button {
-    margin-left: 10px;
-  }
+.modal-enter-active,
+.modal-leave-active {
+  transition: opacity 0.3s;
+}
 
-  /* Modal transition */
-  .modal-enter-active,
-  .modal-leave-active {
-    transition: opacity 0.3s;
-  }
-
-  .modal-enter,
-  .modal-leave-to {
-    opacity: 0;
-  }
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+}
 </style>
