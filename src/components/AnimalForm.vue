ceeed351134cae6494b6f01af6565d4751b5a7a5
Aristevao <aristevao@hotmail.com>
2024-07-17 21:03:08 -0300

    Format document

--- src/components/AnimalForm.vue
+++ src/components/AnimalForm.vue
@@ -42,6 +42,7 @@
           <input type="file" @change="handleFileUpload" />
 
           <div class="button-group">
+            <!-- <button @click="deleteAnimal(animal.id)">Delete</button> -->
             <button type="submit">Save</button>
             <button type="button" @click="cancelForm">Cancel</button>
           </div>
@@ -52,61 +53,62 @@
 </template>
 
 <script>
-import axios from 'axios';
+  import axios from 'axios';
 
-export default {
-  props: {
-    animalData: {
-      type: Object,
-      default: () => ({}),
-    },
-  },
-  data() {
-    return {
-      isVisible: false,
-      units: [],
-      formData: {
-        id: null,
-        name: '',
-        identification: '',
-        specie: '',
-        breed: '',
-        sex: '',
-        birthdate: '',
-        registrationDate: '',
-        description: '',
-        unitId: '',
-        picture: null,
-      },
-    };
-  },
-  watch: {
-    animalData: {
-      immediate: true,
-      handler(newValue) {
-        if (newValue && Object.keys(newValue).length > 0) {
-          this.formData = {
-            id: newValue.id || null,
-            name: newValue.name || '',
-            identification: newValue.identification || '',
-            specie: newValue.specie || '',
-            breed: newValue.breed || '',
-            sex: newValue.sex || '',
-            birthdate: newValue.birthdate || '',
-            registrationDate: newValue.registrationDate || '',
-            description: newValue.description || '',
-            unitId: newValue.unit ? newValue.unit.id : '',
-            picture: null,
-          };
-        }
+  export default {
+    props: {
+      animalData: {
+        type: Object,
+        default: () => ({}),
       },
     },
-  },
-  methods: {
-    open(animal) {
-      this.isVisible = true;
-      this.fetchUnits();
-      if (animal) {
+    data() {
+      return {
+        isVisible: false,
+        // deleteButtonIsVisible: false,
+        units: [],
+        formData: {
+          id: null,
+          name: '',
+          identification: '',
+          specie: '',
+          breed: '',
+          sex: '',
+          birthdate: '',
+          registrationDate: '',
+          description: '',
+          unitId: '',
+          picture: null,
+        },
+      };
+    },
+    watch: {
+      animalData: {
+        immediate: true,
+        handler(newValue) {
+          if (newValue && Object.keys(newValue).length > 0) {
+            this.formData = {
+              id: newValue.id || null,
+              name: newValue.name || '',
+              identification: newValue.identification || '',
+              specie: newValue.specie || '',
+              breed: newValue.breed || '',
+              sex: newValue.sex || '',
+              birthdate: newValue.birthdate || '',
+              registrationDate: newValue.registrationDate || '',
+              description: newValue.description || '',
+              unitId: newValue.unit ? newValue.unit.id : '',
+              picture: null,
+            };
+          }
+        },
+      },
+    },
+    methods: {
+      open(animal) {
+        this.isVisible = true;
+        this.fetchUnits();
+        if (animal) {
           this.formData = {
             id: animal.id,
             name: animal.name,
@@ -123,43 +125,43 @@ export default {
         } else {
           this.resetForm();
         }
-    },
-    close() {
-      this.isVisible = false;
-      this.resetForm();
-    },
-    fetchUnits() {
-      axios.get('http://localhost:8080/api/digital-pec/unit/list')
-        .then(response => {
-          this.units = response.data;
-        })
-        .catch(error => {
-          console.error('Error fetching units:', error);
-        });
-    },
-    handleFileUpload(event) {
-      this.formData.picture = event.target.files[0];
-    },
-    submitForm() {
-      const formData = new FormData();
-      formData.append('name', this.formData.name);
-      formData.append('identification', this.formData.identification);
-      formData.append('specie', this.formData.specie);
-      formData.append('breed', this.formData.breed);
-      formData.append('sex', this.formData.sex);
-      formData.append('birthdate', this.formData.birthdate);
-      formData.append('registrationDate', this.formData.registrationDate);
-      formData.append('description', this.formData.description);
-      formData.append('unit.id', this.formData.unitId);
-      if (this.formData.picture) {
-        formData.append('picture', this.formData.picture);
-      }
+      },
+      close() {
+        this.isVisible = false;
+        this.resetForm();
+      },
+      fetchUnits() {
+        axios.get('http://localhost:8080/api/digital-pec/unit/list')
+          .then(response => {
+            this.units = response.data;
+          })
+          .catch(error => {
+            console.error('Error fetching units:', error);
+          });
+      },
+      handleFileUpload(event) {
+        this.formData.picture = event.target.files[0];
+      },
+      submitForm() {
+        const formData = new FormData();
+        formData.append('name', this.formData.name);
+        formData.append('identification', this.formData.identification);
+        formData.append('specie', this.formData.specie);
+        formData.append('breed', this.formData.breed);
+        formData.append('sex', this.formData.sex);
+        formData.append('birthdate', this.formData.birthdate);
+        formData.append('registrationDate', this.formData.registrationDate);
+        formData.append('description', this.formData.description);
+        formData.append('unit.id', this.formData.unitId);
+        if (this.formData.picture) {
+          formData.append('picture', this.formData.picture);
+        }
 
-      const url = this.formData.id
+        const url = this.formData.id
           ? `http://localhost:8080/api/digital-pec/animal/${this.formData.id}`
           : 'http://localhost:8080/api/digital-pec/animal';
         const method = this.formData.id ? 'put' : 'post';
-  
+
         axios({
           method,
           url,
@@ -168,75 +170,75 @@ export default {
             'Content-Type': 'multipart/form-data',
           },
         })
-      .then(response => {
-        this.$emit('animal-created', response.data);
+          .then(response => {
+            this.$emit('animal-created', response.data);
+            this.close();
+          })
+          .catch(error => {
+            console.error('Error creating animal:', error);
+          });
+      },
+      cancelForm() {
         this.close();
-      })
-      .catch(error => {
-        console.error('Error creating animal:', error);
-      });
+      },
+      resetForm() {
+        this.formData = {
+          id: null,
+          name: '',
+          identification: '',
+          specie: '',
+          breed: '',
+          sex: '',
+          birthdate: '',
+          registrationDate: '',
+          description: '',
+          unitId: '',
+          picture: null,
+        };
+      },
     },
-    cancelForm() {
-      this.close();
-    },
-    resetForm() {
-      this.formData = {
-        id: null,
-        name: '',
-        identification: '',
-        specie: '',
-        breed: '',
-        sex: '',
-        birthdate: '',
-        registrationDate: '',
-        description: '',
-        unitId: '',
-        picture: null,
-      };
-    },
-  },
-};
+  };
 </script>
 
 <style scoped>
-.animal-form-modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 999;
-}
+  .animal-form-modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+  }
 
-.animal-form {
-  background-color: white;
-  padding: 20px;
-  border-radius: 8px;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-  max-width: 400px;
-  width: 100%;
-}
+  .animal-form {
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    max-width: 400px;
+    width: 100%;
+  }
 
-.button-group {
-  margin-top: 20px;
-  text-align: right;
-}
+  .button-group {
+    margin-top: 20px;
+    text-align: right;
+  }
 
-.button-group button {
-  margin-left: 10px;
-}
+  .button-group button {
+    margin-left: 10px;
+  }
 
-.modal-enter-active,
-.modal-leave-active {
-  transition: opacity 0.3s;
-}
+  .modal-enter-active,
+  .modal-leave-active {
+    transition: opacity 0.3s;
+  }
 
-.modal-enter,
-.modal-leave-to {
-  opacity: 0;
-}
+  .modal-enter,
+  .modal-leave-to {
+    opacity: 0;
+  }
 </style>
