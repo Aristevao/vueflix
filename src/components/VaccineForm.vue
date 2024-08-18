50c505aba35463526a8b55309183b869175af5a6
Aristevao <aristevao@hotmail.com>
2024-08-17 21:52:49 -0300

    Add vaccine list

---
+++ src/components/VaccineForm.vue
@@ -0,0 +1,268 @@
+<template>
+  <transition name="modal">
+    <div v-if="isVisible" class="animal-form-modal">
+      <div class="animal-form">
+        <h2>{{ formData.id ? 'Edit Animal' : 'Create New Animal' }}</h2>
+        <form @submit.prevent="submitForm" enctype="multipart/form-data">
+          <label>Name:</label>
+          <input v-model="formData.name" maxlength="80" />
+
+          <label>Identification:</label>
+          <input v-model="formData.identification" required maxlength="80" />
+
+          <label>Specie:</label>
+          <input v-model="formData.specie" maxlength="80" />
+
+          <label>Breed:</label>
+          <input v-model="formData.breed" maxlength="80" />
+
+          <label>Sex:</label>
+          <select v-model="formData.sex">
+            <option>MALE</option>
+            <option>FEMALE</option>
+          </select>
+
+          <label>Birthdate:</label>
+          <input type="date" v-model="formData.birthdate" />
+
+          <label>Registration Date:</label>
+          <input type="date" v-model="formData.registrationDate" />
+
+          <label>Description:</label>
+          <textarea v-model="formData.description" maxlength="500"></textarea>
+
+          <label>Unit:</label>
+          <select v-model="formData.unitId" required>
+            <option v-for="unit in units" :key="unit.id" :value="unit.id">
+              {{ unit.name }}
+            </option>
+          </select>
+
+          <label>Picture:</label>
+          <input type="file" @change="handleFileUpload" />
+
+          <div class="button-group">
+            <button v-if="deleteButtonIsVisible" type="button" @click="deleteAnimal(formData.id)">Delete</button>
+            <button type="submit">Save</button>
+            <button type="button" @click="cancelForm">Cancel</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    props: {
+      animalData: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+    data() {
+      return {
+        isVisible: false,
+        deleteButtonIsVisible: false,
+        units: [],
+        formData: {
+          id: null,
+          name: '',
+          identification: '',
+          specie: '',
+          breed: '',
+          sex: '',
+          birthdate: '',
+          registrationDate: '',
+          description: '',
+          unitId: '',
+          picture: null,
+        },
+      };
+    },
+    watch: {
+      animalData: {
+        immediate: true,
+        handler(newValue) {
+          if (newValue && Object.keys(newValue).length > 0) {
+            this.formData = {
+              id: newValue.id || null,
+              name: newValue.name || '',
+              identification: newValue.identification || '',
+              specie: newValue.specie || '',
+              breed: newValue.breed || '',
+              sex: newValue.sex || '',
+              birthdate: newValue.birthdate || '',
+              registrationDate: newValue.registrationDate || '',
+              description: newValue.description || '',
+              unitId: newValue.unit ? newValue.unit.id : '',
+              picture: null,
+            };
+          }
+        },
+      },
+    },
+    methods: {
+      open(animal) {
+        this.isVisible = true;
+        this.fetchUnits();
+        document.addEventListener('keydown', this.handleKeydown);
+
+        if (animal) {
+          this.deleteButtonIsVisible = true;
+
+          this.formData = {
+            id: animal.id,
+            name: animal.name,
+            identification: animal.identification,
+            specie: animal.specie,
+            breed: animal.breed,
+            sex: animal.sex,
+            birthdate: animal.birthdate,
+            registrationDate: animal.registrationDate,
+            description: animal.description,
+            unitId: animal.unit.id,
+            picture: null,
+          };
+        } else {
+          this.resetForm();
+        }
+      },
+      close() {
+        this.isVisible = false;
+        this.deleteButtonIsVisible = false;
+        this.resetForm();
+        document.removeEventListener('keydown', this.handleKeydown);
+      },
+      handleKeydown(event) {
+        if (event.key === 'Escape') {
+          this.close();
+        }
+      },
+      fetchUnits() {
+        axios.get('http://localhost:8080/api/digital-pec/unit/list')
+          .then(response => {
+            this.units = response.data;
+          })
+          .catch(error => {
+            console.error('Error fetching units:', error);
+          });
+      },
+      handleFileUpload(event) {
+        this.formData.picture = event.target.files[0];
+      },
+      submitForm() {
+        const formData = new FormData();
+        formData.append('name', this.formData.name);
+        formData.append('identification', this.formData.identification);
+        formData.append('specie', this.formData.specie);
+        formData.append('breed', this.formData.breed);
+        formData.append('sex', this.formData.sex);
+        formData.append('birthdate', this.formData.birthdate);
+        formData.append('registrationDate', this.formData.registrationDate);
+        formData.append('description', this.formData.description);
+        formData.append('unit.id', this.formData.unitId);
+        if (this.formData.picture) {
+          formData.append('picture', this.formData.picture);
+        }
+
+        const url = this.formData.id
+          ? `http://localhost:8080/api/digital-pec/animal/${this.formData.id}`
+          : 'http://localhost:8080/api/digital-pec/animal';
+        const method = this.formData.id ? 'put' : 'post';
+
+        axios({
+          method,
+          url,
+          data: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+          .then(response => {
+            this.$emit('animal-created', response.data);
+            this.close();
+          })
+          .catch(error => {
+            console.error('Error creating animal:', error);
+          });
+      },
+      deleteAnimal(animalId) {
+        axios.delete(`http://localhost:8080/api/digital-pec/animal/${animalId}`)
+          .then(() => {
+            this.$emit('animal-deleted', animalId);
+            this.close();
+          })
+          .catch(error => {
+            console.error('Error deleting animal:', error);
+          });
+      },
+      cancelForm() {
+        this.close();
+      },
+      resetForm() {
+        this.formData = {
+          id: null,
+          name: '',
+          identification: '',
+          specie: '',
+          breed: '',
+          sex: '',
+          birthdate: '',
+          registrationDate: '',
+          description: '',
+          unitId: '',
+          picture: null,
+        };
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .animal-form-modal {
+    /* flex-direction: column;  */
+    /* row-gap: 1rem; */
+    
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: 999;
+  }
+
+  .animal-form {
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+    max-width: 600px;
+    width: 100%;
+  }
+
+  .button-group {
+    margin-top: 20px;
+    text-align: right;
+  }
+
+  .button-group button {
+    margin-left: 10px;
+  }
+
+  .modal-enter-active,
+  .modal-leave-active {
+    transition: opacity 0.3s;
+  }
+
+  .modal-enter,
+  .modal-leave-to {
+    opacity: 0;
+  }
+</style>
--- src/components/Vacinas.vue
+++ src/components/Vacinas.vue
@@ -1,12 +1,321 @@
 <template>
-    <div>
-      <h1>Vacinas</h1>
+  <div>
+    <!-- Title and toggle button container -->
+    <div class="header">
+      <div class="title">Vaccines</div>
+      <CustomButton @click="toggleFilters" type="primary" class="toggle-filters-button">
+        {{ showFilters ? 'Hide Filters' : 'Show Filters' }}
+      </CustomButton>
+      <button @click="openVaccineForm" class="add-new-button">
+        Add New
+      </button>
     </div>
-  </template>
-  
-  <script>
-  export default {
+
+    <!-- Filter inputs -->
+    <div v-if="showFilters">
+      <div class="filters">
+        <div class="filter-item">
+          <input v-model="filters.name" placeholder="Filter by Name" class="filter-input" />
+        </div>
+        <div class="filter-item">
+          <input v-model="filters.species" placeholder="Filter by Species" class="filter-input" />
+        </div>
+        <div class="filter-item">
+          <input v-model="filters.description" placeholder="Filter by Description" class="filter-input" />
+        </div>
+      </div>
+      <div class="filter-buttons">
+        <CustomButton @click="clearFilters" type="secondary">Clear Filters</CustomButton>
+        <CustomButton @click="fetchVaccines" type="primary">Apply Filters</CustomButton>
+      </div>
+    </div>
+
+    <!-- VaccineForm component -->
+    <VaccineForm ref="vaccineForm" @vaccine-created="handleVaccineCreated" @vaccine-deleted="handleVaccineDeleted" />
+
+    <!-- Table and pagination components -->
+    <table class="vaccine-table">
+      <!-- Table header -->
+      <thead>
+        <tr>
+          <th class="name-column">Name</th>
+          <th class="species-column">Species</th>
+          <th class="description-column">Description</th>
+        </tr>
+      </thead>
+      <!-- Table body -->
+      <tbody>
+        <tr v-for="vaccine in vaccines" :key="vaccine.id" @click="openVaccineDetails(vaccine.id)"
+          @mouseover="showEllipsis(vaccine.id)" @mouseleave="hideEllipsis(vaccine.id)">
+          <td class="name-column">{{ vaccine.name }}</td>
+          <td class="species-column">{{ vaccine.species.map(s => s.name).join(', ') }}</td>
+          <td class="description-column">{{ vaccine.description }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <!-- Pagination component -->
+    <Pagination :currentPage="currentPage" :totalPages="totalPages" @page-change="handlePageChange" />
+  </div>
+</template>
+
+<script>
+  import { defineComponent } from 'vue';
+  import axios from 'axios';
+  import Pagination from './Pagination.vue';
+  import CustomButton from './CustomButton.vue';
+  import VaccineForm from './VaccineForm.vue';
+
+  export default defineComponent({
     name: 'Vacinas',
-  };
-  </script>
-  
+    components: {
+      Pagination,
+      CustomButton,
+      VaccineForm,
+    },
+    data() {
+      return {
+        vaccines: [],
+        currentPage: 1,
+        totalPages: 0,
+        filters: {
+          name: '',
+          species: '',
+          description: ''
+        },
+        showFilters: false,
+        showVaccineForm: false,
+      };
+    },
+    mounted() {
+      this.fetchVaccines();
+    },
+    methods: {
+      async fetchVaccines() {
+        try {
+          const response = await axios.get('http://localhost:8080/api/digital-pec/vaccine', {
+            params: {
+              page: this.currentPage - 1,
+              size: 10,
+              sort: 'name,asc',
+              name: this.filters.name,
+              species: this.filters.species,
+              description: this.filters.description
+            }
+          });
+          this.vaccines = response.data.content;
+          this.totalPages = response.data.totalPages;
+        } catch (error) {
+          console.error(error);
+        }
+      },
+      handlePageChange(newPage) {
+        this.currentPage = newPage;
+        this.fetchVaccines(); // Fetch data for the new page
+      },
+      calculateDescription(birthdate) {
+        const birthDate = new Date(birthdate);
+        const today = new Date();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+        if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
+          age--;
+        }
+        return `${age} year${age !== 1 ? 's' : ''}`;
+      },
+      clearFilters() {
+        this.filters.identification = '';
+        this.filters.name = '';
+        this.filters.species = '';
+        this.filters.description = '';
+        this.filters.sex = '';
+        this.filters.birthdate = '';
+        this.filters.registrationDate = '';
+        this.fetchVaccines();
+      },
+      toggleFilters() {
+        this.showFilters = !this.showFilters; // Toggle filter visibility
+      },
+      openVaccineForm() {
+        this.$refs.vaccineForm.open();
+      },
+      handleVaccineCreated() {
+        this.fetchVaccines();
+      },
+      handleVaccineDeleted() {
+        this.fetchVaccines();
+      },
+      openVaccineDetails(vaccineId) {
+        axios.get(`http://localhost:8080/api/digital-pec/vaccine/${vaccineId}`)
+          .then(response => {
+            this.$refs.vaccineForm.open(response.data);
+          })
+          .catch(error => {
+            console.error('Error fetching vaccine details:', error);
+          });
+      },
+      deleteVaccine(vaccineId) {
+        axios.delete(`http://localhost:8080/api/digital-pec/vaccine/${vaccineId}`)
+          .then(() => {
+            this.vaccines = this.vaccines.filter(vaccine => vaccine.id !== vaccineId);
+          })
+          .catch(error => {
+            console.error('Error deleting vaccine:', error);
+          });
+      },
+      showEllipsis(vaccineId) {
+        this.vaccines = this.vaccines.map(vaccine => vaccine.id === vaccineId ? { ...vaccine, showEllipsis: true } : vaccine);
+      },
+      hideEllipsis(vaccineId) {
+        this.vaccines = this.vaccines.map(vaccine => vaccine.id === vaccineId ? { ...vaccine, showEllipsis: false, showOptions: false } : vaccine);
+      },
+      toggleOptions(vaccineId) {
+        this.vaccines = this.vaccines.map(vaccine => vaccine.id === vaccineId ? { ...vaccine, showOptions: !vaccine.showOptions } : vaccine);
+      },
+    },
+  });
+</script>
+
+<style scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 10px;
+  }
+
+  .title {
+    font-family: sans-serif;
+    font-size: 24px;
+    font-weight: bold;
+  }
+
+  .toggle-filters-button {
+    margin-bottom: 10px;
+  }
+
+  .filters {
+    margin-bottom: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: left;
+  }
+
+  .filter-item {
+    display: flex;
+    align-items: center;
+  }
+
+  .filter-input {
+    width: 100%;
+    max-width: 150px;
+    padding: 5px;
+    box-sizing: border-box;
+  }
+
+  .filter-buttons {
+    display: flex;
+    gap: 10px;
+    margin-bottom: 10px;
+  }
+
+  .button-group {
+    margin-top: 20px;
+    text-align: right;
+  }
+
+  .button-group button {
+    margin-left: 10px;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    padding: 10px;
+    position: relative;
+  }
+
+  .ellipsis {
+    cursor: pointer;
+    float: right;
+    /* width: 150px; */
+  }
+
+  .options {
+    display: flex;
+    gap: 10px;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+
+  .options button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    color: blue;
+    text-decoration: underline;
+  }
+
+  .vaccine-table {
+    width: 100%;
+    border-collapse: separate;
+    border-spacing: 0px 0;
+    font-family: sans-serif;
+  }
+
+  .vaccine-table th,
+  .vaccine-table td {
+    padding: 10px;
+    text-align: left;
+  }
+
+  .vaccine-table th {
+    /* background-color: #f2f2f2; */
+  }
+
+  .vaccine-table tr:nth-child(even) {
+    background-color: #f9f9f9;
+  }
+
+  .vaccine-table img {
+    border-radius: 17%;
+  }
+
+  .imdescription-column {
+    width: 50px;
+    text-align: center;
+  }
+
+  .identification-column {
+    width: 50px;
+  }
+
+  .name-column {
+    width: 120px;
+  }
+
+  .species-column {
+    width: 100px;
+  }
+
+  .breed-column {
+    width: 80px;
+  }
+
+  .sex-column {
+    width: 50px;
+  }
+
+  .description-column {
+    width: 80px;
+  }
+
+  .registration-date-column {
+    width: 150px;
+  }
+</style>
