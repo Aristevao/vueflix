b75640ef884da9991a77655bef03480d58c560d8
Aristevao <aristevao@hotmail.com>
2024-09-08 00:19:05 -0300

    add global css to vaccine and application

--- src/components/AnimalVaccineForm.vue
+++ src/components/AnimalVaccineForm.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="modal">
-    <div v-if="isVisible" class="animalVaccine-form-modal" @click="handleBackgroundClick">
-      <div class="animalVaccine-form" @click.stop>
+    <div v-if="isVisible" class="entity-form-modal" @click="handleBackgroundClick">
+      <div class="entity-form" @click.stop>
         <span class="close-button" @click="close">&times;</span>
         <h2>{{ formData.id ? 'Edit Vaccine' : 'Create New Vaccine' }}</h2>
         <form @submit.prevent="submitForm" enctype="multipart/form-data">
@@ -190,101 +190,11 @@
 </script>
 
 <style scoped>
-  .animalVaccine-form-modal {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.5);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    z-index: 999;
-  }
-
-  .animalVaccine-form {
-    background-color: white;
-    padding: 20px;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-    max-width: 600px;
-    width: 100%;
-    position: relative;
-  }
-
-  .close-button {
-    position: absolute;
-    top: 5px;
-    right: 10px;
-    font-size: 24px;
-    cursor: pointer;
-  }
-
-  .form-group {
-    margin-bottom: 15px;
-  }
-
-  .form-group label {
-    display: block;
-    margin-bottom: 5px;
-    font-weight: bold;
-  }
+  @import "@/assets/form-styles.css";
 
   .form-completed {
     display: block;
     margin-bottom: 15px;
     font-weight: bold;
-  }
-
-  .form-group input,
-  .form-group textarea {
-    width: 100%;
-    padding: 8px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-
-  .species-group {
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-  }
-
-  .species-group input {
-    flex: 1;
-    margin-right: 10px;
-  }
-
-  .button-group {
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-    margin-top: 20px;
-  }
-
-  .right-buttons {
-    display: flex;
-    align-items: center;
-    margin-left: auto;
-  }
-
-  .right-buttons button {
-    margin-left: 10px;
-  }
-
-  .delete-button {
-    margin-right: auto;
-  }
-
-  .modal-enter-active,
-  .modal-leave-active {
-    transition: opacity 0.3s;
-  }
-
-  .modal-enter,
-  .modal-leave-to {
-    opacity: 0;
   }
 </style>
