e42c386d4efa433f64642b2f8a094409d2946462
Aristevao <aristevao@hotmail.com>
2024-09-15 10:57:47 -0300

    Fix timezone

--- src/components/NotificationList.vue
+++ src/components/NotificationList.vue
@@ -127,7 +127,16 @@
         }
       },
       formatDate(date) {
-        return new Date(date).toLocaleString('pt-BR', { timeZone: 'UTC', hour12: false })
+        const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        return new Date(date + "Z").toLocaleString('pt-BR', {
+          timeZone: userTimeZone,
+          hour12: false,
+          year: 'numeric',
+          month: '2-digit',
+          day: '2-digit',
+          hour: '2-digit',
+          minute: '2-digit',
+        });
       },
       truncateMessage(message) {
         return message.length > 100 ? `${message.substring(0, 100)}...` : message
